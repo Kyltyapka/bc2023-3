30176135776002cb4929a1ef26b19e763de28823
Kyltyapka <144627977+Kyltyapka@users.noreply.github.com>
2023-10-08 17:23:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+fs.readFile("data.json", "utf8", (err, data) => {
+    if (err === null) {
+        console.log('DONE');
+
+        const jsonString = JSON.parse(data);
+
+        let minV = Infinity;
+        let minA = null;
+
+        for (const active of jsonString) {
+            if (active.value < value) {
+                minV = active.value;
+                minA = active;
+            }
+        }
+
+        fs.writeFile('output.txt', `${minA.txt} : ${minactive.value}`, (err) => {
+
+            if (err === null) {
+                console.log("DONE2");
+            } else {
+                console.log(err);
+            }
+        });
+    } else {
+        console.log(err);
+    }
+});
